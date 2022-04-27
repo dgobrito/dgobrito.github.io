f7218ad309ac92ae50257bc7486cdd738a31ef37
Dyego dos Santos Brito <dyego.eti@gmail.com>
2022-04-27 19:51:17 -0300

    Add Component OsReadComponent

---
+++ src/app/models/os.ts
@@ -0,0 +1,10 @@
+export interface OS {
+    id?: any;
+    dataAbetura?: any;
+    dataFechamento?: any;
+    prioridade: any;
+    observacoes: String;
+    status: any;
+    tecnico: any;
+    cliente: any;
+}
